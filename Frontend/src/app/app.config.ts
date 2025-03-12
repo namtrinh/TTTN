bee6730ff8e0449a24305d2cd476f626ceebd675
namcao <cunnconn01@gmail.com>
2025-03-12 11:45:12 +0700

    a

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,22 +1,14 @@
-import {ApplicationConfig, EnvironmentProviders, InjectionToken, provideZoneChangeDetection} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {HTTP_INTERCEPTORS, HttpInterceptor, provideHttpClient} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, provideHttpClient} from '@angular/common/http';
 import {AuthInterceptor} from '../service/auth.interceptor';
-import {BrowserDynamicTestingModule} from '@angular/platform-browser-dynamic/testing';
 
-export const appConfig: {
-  providers: (EnvironmentProviders | BrowserDynamicTestingModule | {
-    useClass: AuthInterceptor;
-    provide: InjectionToken<readonly HttpInterceptor[]>;
-    multi: boolean
-  })[]
-} = {
+export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    BrowserDynamicTestingModule,
     {
       provide:HTTP_INTERCEPTORS,
       useClass:AuthInterceptor,
